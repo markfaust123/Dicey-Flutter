1428c86313e3a14bd7afa43a96c01169d6aa6a28
Mark Faust <markfaust123@gmail.com>
2023-07-11 21:51:27 -0400

    completed Dicey project with both dice udpating randomly upon their press

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +7,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text('Dicey'),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +16,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // Everything within the build function will
+    // be updated during HotReload!!!
+
+    void updateDice() {
+      setState(() {
+        leftDiceNumber = Random().nextInt(6) + 1;
+        rightDiceNumber = Random().nextInt(6) + 1;
+      });
+    }
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: IconButton(
+              // This is called an "anonymous" function (AKA VoidCallback)
+              onPressed: updateDice,
+              iconSize: 170.0,
+              icon: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: IconButton(
+              onPressed: updateDice,
+              iconSize: 170.0,
+              icon: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
